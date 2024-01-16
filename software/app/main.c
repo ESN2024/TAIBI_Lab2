3213a93e68d44119ba221e41a9d7a74e3dbfd6e8
TAIBIAchraf <114927888+TAIBIAchraf@users.noreply.github.com>
2024-01-16 23:08:12 +0100

    Compteur de 0 a 999 sur les trois digits

--- software/app/main.c
+++ software/app/main.c
@@ -4,18 +4,28 @@
 #include "unistd.h"
 
 int main(){
-volatile  unsigned char one = 0x01;
+volatile  unsigned short number = 0x01;
+volatile  unsigned short unites = 0x00;
+volatile  unsigned short dizaines = 0x00;
+volatile  unsigned short centaines = 0x00;
+
+   
+
 while(1){
-	if(one > 9){
-		one = 0000;
+    unites = number % 10;
+    dizaines = (number / 10) % 10; 
+    centaines = number / 100;
+	if(number <= 999)
+	{
+		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_1_BASE,unites);
+		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_2_BASE,dizaines);
+		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_3_BASE,centaines);
+		usleep(10000);
+		number = number + 1;
 	}
-	else {
-		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_1_BASE,one);
-		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_2_BASE,one);
-		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_3_BASE,one);
-		usleep(100000);
-		one = one + 1;
-		}
+	else{
+		number = 0x00;
+	}
 
 }
 }
