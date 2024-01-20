7dcb417aa2038750b2ea6db6580ba15707648024
TAIBIAchraf <114927888+TAIBIAchraf@users.noreply.github.com>
2024-01-20 12:30:25 +0100

    Refactoring du code + supression fichiers inutiles

--- software/app/main.c
+++ software/app/main.c
@@ -8,30 +8,38 @@
 #include "sys/alt_irq.h"
 #include "sys/alt_sys_init.h"
 
-//variables globales
+//Variables utiles
 volatile  unsigned short number = 0x01;
-volatile  unsigned short unites = 0x00;
-volatile  unsigned short dizaines = 0x00;
-volatile  unsigned short centaines = 0x00;
+volatile  unsigned short unites,dizaines,centaines = 0x00;
+
 
 
 // La fonction Interrupt Sub-Routine
 static void handle_interrupts(void* context)
 {   
+	//Enregistrer les unités
 	unites = number % 10;
+	//Enregistrer les dizaines
     dizaines = (number / 10) % 10; 
+	//Enregistrer centaines
     centaines = number / 100;
+
 	if(number <= 999)
 	{
+		// Ecrire dans l'afficheur 7seg numéro 1
 		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_1_BASE,unites);
+		// Ecrire dans l'afficheur 7seg numéro 2
 		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_2_BASE,dizaines);
+		// Ecrire dans l'afficheur 7seg numéro 3
 		IOWR_ALTERA_AVALON_PIO_DATA(DIGIT_3_BASE,centaines);
+		// Incrémenter
 		number = number + 1;
 	}
+	// On rénitialise le compteur à 999
 	else{
 		number = 0x00;
 	}
-	// Clear the interrupt
+	// Ecrire dans le registre status pour réinitialiser l'interruption
 	IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0);
 
 }
@@ -42,11 +50,13 @@ static void handle_interrupts(void* context)
 static void init_interruption()
 {
 
-	// Start the timer (continous - start - ito?)
-		//register to HAL
+	//Enregistrement de la fonction ISR handle_interrupts
 	alt_irq_register( TIMER_0_IRQ, NULL, (void*)handle_interrupts);
 
-
+	// Démarrer le timer en utilisant le registre control, les bits activés sont (ito - cont - start)
+	// ALTERA_AVALON_TIMER_CONTROL_ITO_MSK : masque pour gérérer une requète d'interruption 
+	// ALTERA_AVALON_TIMER_CONTROL_START_MSK : masque pour commencer le timer
+	// ALTERA_AVALON_TIMER_CONTROL_CONT_MSK : masque pour mettre le timer en mode continue, l'arret est effectuer par le bit stop du meme registre
 	IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_0_BASE, ALTERA_AVALON_TIMER_CONTROL_CONT_MSK
 			                                   | ALTERA_AVALON_TIMER_CONTROL_START_MSK
 											   | ALTERA_AVALON_TIMER_CONTROL_ITO_MSK);
@@ -56,10 +66,11 @@ static void init_interruption()
 
 
 int main(){
-
-
+//Initialisation
 init_interruption();
+
 while(1){};
+
 return 0;
 
 }
--- software/bsp/linker.x
+++ software/bsp/linker.x
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'TAIBI_Lab2_qsys_file'
  * SOPC Builder design path: ../../TAIBI_Lab2_qsys_file.sopcinfo
  *
- * Generated: Wed Jan 17 00:44:37 CET 2024
+ * Generated: Sat Jan 20 12:26:30 CET 2024
  */
 
 /*
